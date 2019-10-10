db3b87862d1b5106c7e582060382bf783aba0836
Ender Holten <contact@eholten.eu>
2019-10-10 14:44:23 +0200

    Added a few pages to test out the template engine of flask and implemented bootstrap to make it look snappy

--- flasktest.py
+++ flasktest.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author' : 'Ender Holten',
+        'title' : 'Might as well make it myself',
+        'content' : 'Should bookstack fail me then there\'s always Flask',
+        'date' : 'October 10, 2019'
+    },
+    {
+        'author' : 'Ender Holten',
+        'title' : 'Or I\'ll just leave it like this',
+        'content' : 'Considering the fact that my time is limited as it is',
+        'date' : 'October 11, 2019'
+    }
+]
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello, World?</h1>'
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts, title="home")
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="about")
 
 
 if __name__ == '__main__':
+    # Turn debug off when used on server!
     app.run(debug=True)
