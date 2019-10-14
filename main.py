1c124403a9fd888ddbcfc7cc379e987ea787892a
Ender Holten <contact@eholten.eu>
2019-10-14 11:49:59 +0200

    Test commit

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import APIfunctions
 
 # Some Constants
 SEARCHTERM = "infection prevention"
-COUNTRY = "nl"  # us, uk, nl
+COUNTRY = "us"  # us, uk, nl
 
 app_results = APIfunctions.search_both_stores(SEARCHTERM, COUNTRY)
 
 APIfunctions.export_csv(app_results, "output")
+
+
