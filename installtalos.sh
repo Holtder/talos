7b1832ede89cf9cd07ab91e34fc4a3a360d652f4
Ender Holten <contact@eholten.eu>
2020-03-05 15:29:21 +0000

    First draft of install script

--- installtalos.sh
+++ installtalos.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 pyv="$(python3 -V 2>&1)"
 pyv="${pyv:7:5}"
 if [ "$pyv" != "3.6.8" ]; then
@@ -12,14 +17,21 @@ echo " - Git"
 echo " - Python3-pip"
 echo " - Python3-venv"
 echo
+echo
+echo "It will also install redis and nginx on your server. Make sure you have uninstalled any webservers beforehand!"
+echo
 while true; do
     read -p "Is this ok? (Y/N)" yn
     case $yn in
-        [Yy]* ) sudo apt-get install -qq python3-pip python3-venv git || exit 1; break;;
+        [Yy]* ) sudo apt-get install -qq python3-pip python3-venv redis-server git nginx || exit 1; break;;
         [Nn]* ) echo "Exiting Talos installer"; exit;;
         * ) echo "Please answer yes or no. (Y/N)";;
     esac
 done
+
+echo "Enabling redis service"
+systemctl enable redis-server.service
+
 
 echo
 echo
@@ -47,6 +59,85 @@ cd "Talos" || exit 1
 rm "installtalos.sh"
 rm -rf ".git"
 
+cwd=$(pwd)
+
+echo "Creating virtual environment and installing Required Packages"
+
 python3 -m venv .env || exit 1
 source ".env/bin/activate" || exit 1
 pip install -r "requirements.txt" || exit 1
+
+echo "Starting Redis"
+redis-server
+
+echo "Creating service"
+rm -f /etc/systemd/system/talos.service
+cat <<EOT >> /etc/systemd/system/talos.service
+#Metadata and dependencies section
+[Unit]
+Description=Talos service
+After=network.target
+#Define users and app working directory
+[Service]
+User=$USER
+Group=www-data
+WorkingDent==$cwd
+Environment="PATH=$cwd/.env/bin"
+ExecStart=$cwd/.env/bin/uwsgi --ini talos.ini
+#Link the service to start on multi-user system up
+[Install]
+WantedBy=multi-user.target
+EOT
+
+
+echo "Generating supervisord config"
+rm -f supervisord.conf
+echo_supervisord_conf > $cwd/supervisord.conf
+cat <<EOT >> $cwd/supervisord.conf
+[program:celeryd]
+command=$cwd/.env/bin/celery -A entrypoint_celery.celery worker --concurrency=1
+stdout_logfile=$cwd/celeryd.log
+stderr_logfile=$cwd/celeryd.log
+autostart=true
+autorestart=true
+startsecs=10
+stopwaitsecs=600
+
+
+EOT
+
+
+systemctl start talos
+systemctl enable talos
+supervisord
+
+echo "Configuring nginx"
+
+echo "What is the address (domain or ip) you intend to serve Talos from [default=0.0.0.0]:"
+
+read -e -i "0.0.0.0" hostedaddress
+
+rm -f /etc/nginx/sites-available/talos
+rm -f /etc/nginx/sites-enabled/talos
+
+cat <<EOT >> /etc/nginx/sites-available/talos
+
+server {
+    # the port your site will be served on
+    listen 80;
+    # the IP Address your site will be served on
+    server_name $hostedaddress;
+    # Proxy connections to application server
+    location / {
+        include uwsgi_params;
+        uwsgi_pass unix:$cwd/talos.sock;
+    }
+}
+EOT
+
+ln -s /etc/nginx/sites-available/talos /etc/nginx/sites-enabled
+
+echo "Reloading nginx config and restarting nginx"
+ngnix -t
+systemctl restart nginx
+
